fcf20a7892b51b17a987a8ce14c51ee27b26bddd
Neguete10 <henriquemlima@hotmail.com>
2023-03-28 16:14:40 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = require("./routers/router");
 const cors = require("cors");
-
 const connectDB = require("./database/connection");
 connectDB();
 
 const app = express();
+
+//enable cors
+app.use(cors());
 //enable urlencoded capabilities
 app.use(express.urlencoded({ extended: true }));
 //enable json capabilities
 app.use(express.json());
-//enable cors
-app.use(cors());
 //enable the router
 app.use(router);
 
