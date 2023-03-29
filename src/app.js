6309ed01400e2012c1d87a624625042c2178854f
Neguete10 <henriquemlima@hotmail.com>
2023-03-29 16:09:12 -0300

    try again

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-const https = require('https');
-const fs = require('fs');
+//const https = require('https');
+//const fs = require('fs');
 const express = require("express");
 const router = require("./routers/router");
 const cors = require("cors");
 const connectDB = require("./database/connection");
 connectDB();
 
+/*
 const options = {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
 };
-  
+*/
 const app = express();
 //enable cors
 app.use(cors());
@@ -21,8 +22,8 @@ app.use(express.json());
 //enable the router
 app.use(router);
 
-const server = https.createServer(options, app);
+//const server = https.createServer(options, app);
 
 
 
-module.exports = server;
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const server = require("./app");
+const app = require("./app");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () =>
-  console.log(`Server running on https://localhost:${PORT}`)
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
 );
