b6931a429398548400588e18506189120302128e
Neguete10 <henriquemlima@hotmail.com>
2023-03-29 15:36:02 -0300

    ca certificate

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const app = require("./app");
+const server = require("./app");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+server.listen(PORT, () =>
+  console.log(`Server running on https://localhost:${PORT}`)
 );
