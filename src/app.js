51f6de3b7b790a4532c7cf1a3e7ce3c986027cfd
Neguete10 <henriquemlima@hotmail.com>
2023-03-29 17:10:47 -0300

    sign

--- src/app.js
+++ src/app.js
@@ -8,13 +8,25 @@ connectDB();
 
 /*
 const options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
+    key: fs.readFileSync('private.key'),
+    cert: fs.readFileSync('certificate.pem')
 };
 */
 const app = express();
 //enable cors
-app.use(cors());
+/*
+app.use(cors(
+    {
+        origin: "https://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization", "Origin", "X-Requested-With", "Accept"],
+
+    }
+
+
+));
+*/
 //enable urlencoded capabilities
 app.use(express.urlencoded({ extended: true }));
 //enable json capabilities
